3397e919710fc41dcd3d3d82e78dfb6143a190a1
streescult <streescult@disroot.org>
2023-07-22 15:43:44 +0300

    Revert "added anybar patch"
    This reverts commit 535df2b24f608722478e8fcac730cfd0a7683106.

--- config.def.h
+++ config.def.h
@@ -10,12 +10,8 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
-static const char *alttrayname      = "tray";    /* Polybar tray instance name */
-static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
--- config.h
+++ config.h
@@ -3,19 +3,15 @@
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 5;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
-static const char *alttrayname      = "tray";    /* Polybar tray instance name */
-static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -37,11 +33,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class          instance    title           tag     float  mon  float x,y,w,h     floatborderpx*/
-	{ "wee-scratch",   NULL,       NULL,          512,    1,     -1,  15,15,350,420,    2 },
-	{ "news-scratch",  NULL,       NULL,          2048,   1,     -1,  999,15,350,500,   2 },
-	{ "mail-scratch",  NULL,       NULL,          4096,   0,     -1,  50,50,500,500,    2 },
-	{ "fm-scratch",    NULL,       NULL,          8192,   1,     -1,  50,50,500,500,    2 },
-	{ "term-scratch",  NULL,       NULL,          16384,  1,     -1,  50,50,500,500,    2 },
+	{ "weescratch",   NULL,       NULL,          0,    1,     -1,  15,15,550,420,    2 },
+	{ "mailscratch",  NULL,       NULL,          0,   0,     -1,  50,50,500,500,    2 },
+	{ "fmscratch",    NULL,       NULL,          0,   1,     -1,  50,50,500,500,    2 },
+	{ "termscratch",  NULL,       NULL,          0,  1,     -1,  50,50,500,500,    2 },
 };
 
 /* layout(s) */
